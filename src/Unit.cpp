d7207d123c7a5ec78794eeef269f11368cfa4408
tomsloj <tomsloj@gmail.com>
2020-04-20 19:13:01 +0200

    unit drawing

--- src/Unit.cpp
+++ src/Unit.cpp
@@ -1,6 +1,9 @@
 #include "../include/Unit.hpp"
 
+#include <SFML/Graphics.hpp>
 #include <iostream>
+
+using namespace sf;
 
 Unit::Unit(  )
 {
@@ -10,9 +13,17 @@ Unit::Unit(  )
 Unit::Unit( double X, double Y, int size /*= 1*/ )
 {
     std::cout<<"UNIT\n";
-    this->X = X;
-    this->Y = Y;
+    position = sf::Vector2f(X, Y);
     this->size = size;
+    sprite = new CircleShape();
+}
+
+Unit::Unit( Vector2f position, int size /*= 1*/ )
+{
+    std::cout<<"UNIT\n";
+    this->position = position;
+    this->size = size;
+    sprite = new CircleShape();
 }
 
 void Unit::grow(const int sizeOfGrowth)
@@ -28,4 +39,12 @@ void Unit::increaseSize( int sizeReduction /*= 1*/ )
 int Unit::getSize()
 {
     return size;
+}
+
+void Unit::draw(sf::RenderWindow& window)
+{
+    sprite->setPosition(position);
+    sprite->setRadius(size);
+    sprite->setFillColor(color);
+    window.draw(*sprite);
 }
